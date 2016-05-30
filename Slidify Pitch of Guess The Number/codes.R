0f9e3233850f81c2145762a2300c763df351c5c8
Ganesh Shrestha <thimiganesh@gmail.com>
2016-05-30 03:52:37 -0600

    Added files for Guess the Number

---
+++ Slidify Pitch of Guess The Number/codes.R
@@ -0,0 +1,11 @@
+#install and load devtools
+install.packages("devtools")
+library(devtools)
+
+#install and load Slidify
+install_github('slidify', 'gashrestha')
+install_github('slidifyLibraries', 'gashrestha')
+library(slidify)
+
+author("Guess_The_Number")
+
